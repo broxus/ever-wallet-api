3a271cba742fb6545a1564cda4b6a503514ebebb
Alexey Pashinov <pashinov93@gmail.com>
2021-09-15 15:06:29 +0300

    Fix token owners indexes

--- migrations/20210811140022_token_owners.sql
+++ migrations/20210811140022_token_owners.sql
@@ -10,6 +10,4 @@ CREATE TABLE token_owners
     UNIQUE (owner_account_workchain_id, owner_account_hex, root_address)
 );
 
-CREATE INDEX token_owners_account_workchain_id_idx ON token_owners (owner_account_workchain_id);
-CREATE INDEX token_owners_account_hex_idx ON token_owners (owner_account_hex);
-CREATE INDEX token_owners_root_address_idx ON token_owners (root_address);
+CREATE INDEX token_owners_address_idx ON token_owners (owner_account_workchain_id, owner_account_hex, root_address);
