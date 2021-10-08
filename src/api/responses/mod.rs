eda1b530c10a7045325326bde707b016d9e07028
serejkaaa512 <sefremow@dexpa.io>
2021-10-08 11:23:39 +0300

    improved transaction response

--- src/api/responses/mod.rs
+++ src/api/responses/mod.rs
@@ -370,24 +370,85 @@ pub struct AccountTransactionDataResponse {
     pub message_hash: String,
     pub transaction_hash: Option<String>,
     pub transaction_lt: Option<String>,
+    pub transaction_timeout: Option<i64>,
+    #[opg("UTC timestamp in milliseconds", integer, format = "int64")]
+    pub transaction_timestamp: Option<i64>,
     pub account: AddressResponse,
+    pub sender: Option<AddressResponse>,
+    pub data: Option<AccountTransactionData>,
     #[opg("value", string)]
     pub value: Option<BigDecimal>,
+    #[opg("originalValue", string)]
+    pub original_value: Option<BigDecimal>,
+    #[opg("fee", string)]
+    pub fee: Option<BigDecimal>,
     #[opg("balanceСhange", string)]
     pub balance_change: BigDecimal,
+    pub out_messages: Option<Vec<AccountTransactionMessage>>,
+    pub original_outputs: Option<Vec<AccountTransactionOutput>>,
     pub direction: TonTransactionDirection,
     pub status: TonTransactionStatus,
     pub aborted: bool,
     pub bounce: bool,
     pub sender_is_token_wallet: bool,
+    pub error: Option<String>,
     #[opg("UTC timestamp in milliseconds", integer, format = "int64")]
     pub created_at: i64,
     #[opg("UTC timestamp in milliseconds", integer, format = "int64")]
     pub updated_at: i64,
 }
 
+#[derive(Debug, Deserialize, Serialize, Clone, opg::OpgModel)]
+#[opg("AccountTransactionData")]
+pub enum AccountTransactionData {
+    SafeMultisig,
+    Other,
+}
+
 impl From<TransactionDb> for AccountTransactionDataResponse {
     fn from(c: TransactionDb) -> Self {
+        let sender = if let (Some(sender_hex), Some(sender_workchain_id)) =
+            (c.sender_hex, c.sender_workchain_id)
+        {
+            let sender =
+                MsgAddressInt::from_str(&format!("{}:{}", sender_workchain_id, sender_hex))
+                    .unwrap();
+            let sender_base64url = Address(pack_std_smc_addr(true, &sender, false).unwrap());
+            Some(AddressResponse {
+                workchain_id: sender_workchain_id,
+                hex: Address(sender_hex),
+                base64url: sender_base64url,
+            })
+        } else {
+            None
+        };
+
+        let original_outputs = if let Some(outputs) = c.original_outputs {
+            let original_outputs: Vec<TransactionSendOutput> =
+                serde_json::from_value(outputs).unwrap_or_default();
+            Some(
+                original_outputs
+                    .into_iter()
+                    .map(|output| {
+                        let output_address =
+                            nekoton_utils::repack_address(&output.recipient_address.0).unwrap();
+                        let output_base64url =
+                            Address(pack_std_smc_addr(true, &output_address, false).unwrap());
+                        AccountTransactionOutput {
+                            value: output.value.to_string(),
+                            recipient: AddressResponse {
+                                workchain_id: output_address.workchain_id(),
+                                hex: Address(output_address.address().to_hex_string()),
+                                base64url: output_base64url,
+                            },
+                        }
+                    })
+                    .collect(),
+            )
+        } else {
+            None
+        };
+
         let account =
             MsgAddressInt::from_str(&format!("{}:{}", c.account_workchain_id, c.account_hex))
                 .unwrap();
@@ -398,22 +459,49 @@ impl From<TransactionDb> for AccountTransactionDataResponse {
             message_hash: c.message_hash,
             transaction_hash: c.transaction_hash,
             transaction_lt: c.transaction_lt.map(|v| v.to_string()),
+            transaction_timeout: c.transaction_timeout,
             account: AddressResponse {
                 workchain_id: c.account_workchain_id,
                 hex: Address(c.account_hex),
                 base64url,
             },
+            sender,
+            data: None,
             value: c.value,
+            original_value: c.original_value,
+            fee: c.fee,
             balance_change: c.balance_change.unwrap_or_default(),
+            out_messages: c.messages.and_then(|m| serde_json::from_value(m).ok()),
+            original_outputs,
             direction: c.direction,
             status: c.status,
             aborted: c.aborted,
             bounce: c.bounce,
             sender_is_token_wallet: c.sender_is_token_wallet,
+            transaction_timestamp: c.transaction_timestamp.map(|t| t.timestamp()),
             created_at: c.created_at.timestamp_millis(),
             updated_at: c.updated_at.timestamp_millis(),
+            error: c.error,
         }
     }
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone, opg::OpgModel)]
+#[serde(rename_all = "camelCase")]
+#[opg("AccountTransactionMessage")]
+pub struct AccountTransactionMessage {
+    pub message_hash: String,
+    pub value: String,
+    pub fee: String,
+    pub recipient: AddressResponse,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone, opg::OpgModel)]
+#[serde(rename_all = "camelCase")]
+#[opg("AccountTransactionOutput")]
+pub struct AccountTransactionOutput {
+    pub value: String,
+    pub recipient: AddressResponse,
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone, opg::OpgModel)]
