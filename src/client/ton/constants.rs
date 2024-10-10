5e32c08876e9dcbac7cd0f9a17fd345736017e43
Ilya Evdokimov <evdokimov.ilya@gmail.com>
2024-10-10 22:59:45 +0200

    chain ids, constants re-arranged

--- src/client/ton/constants.rs
+++ src/client/ton/constants.rs
@@ -1,15 +1,15 @@
 use std::str::FromStr;
-use ton_types::UInt256; // Replace with the actual crate providing `UInt256`
+use ton_types::UInt256;
 
 pub fn get_network_id(root_hash: UInt256) -> i32 {
-    let EVER_ID = UInt256::from_str("WP/KGheNr/cF3lQhblQzyb0ufYUAcNM004mXhHq56EU=").unwrap();
-    let VENOM_ID = UInt256::from_str("YLICYJzBkBm9C7RjszRzr7sUv/VsDkdaibI+baqLahA=").unwrap();
-    let TON_ID = UInt256::from_str("F6OpKZKqvqeFp6CQmFomXNMfMj2EnaUSOXN+Mh+wVWk=").unwrap();
-    if root_hash == EVER_ID {
+    let ever = UInt256::from_str("WP/KGheNr/cF3lQhblQzyb0ufYUAcNM004mXhHq56EU=").unwrap();
+    let venom = UInt256::from_str("YLICYJzBkBm9C7RjszRzr7sUv/VsDkdaibI+baqLahA=").unwrap();
+    let ton = UInt256::from_str("F6OpKZKqvqeFp6CQmFomXNMfMj2EnaUSOXN+Mh+wVWk=").unwrap();
+    if root_hash == ever {
         42
-    } else if root_hash == VENOM_ID {
+    } else if root_hash == venom {
         1
-    } else if root_hash == TON_ID {
+    } else if root_hash == ton {
         -239
     } else {
         0
--- src/client/ton/mod.rs
+++ src/client/ton/mod.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+
 use std::sync::Arc;
 use std::sync::atomic::Ordering;
 
